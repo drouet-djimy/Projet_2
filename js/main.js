a7924bc8b847722f7c93c9bd0fbefad99dd7118f
corentin <corentin.guillard@ynov.com>
2018-12-12 16:15:44 +0100

    implémentation de Jqueri + ajout de la partie code érreur de la géolocalisation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 function getLocation() {
-  var x = document.getElementById("demo");
+  var x = document.getElementById("affichage");
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    x.innerHTML = "La géolocalisation n'est pas prise en charge par ce navigateur.";
   }
 }
 
 function showPosition(position) {
-  var x = document.getElementById("demo");
+  var x = document.getElementById("affichage");
   x.innerHTML = "Latitude: <br>" + position.coords.latitude +
     "<br><br>Longitude: <br>" + position.coords.longitude;
+}
+
+function showError(error) {
+  var x = document.getElementById("affichage");
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "Vous avez refusé la demande de géolocalisation. <br>Veillez activer votre géolocalisation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "La localisation est indisponibles."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "Votre demande d'emplacement a expiré."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "Une erreur inconnue est survenue."
+      break;
+  }
 }
